4688817aee332cc8694b0ccdec176314d509192c
Dioswoah <103057088+Dioswoah@users.noreply.github.com>
2025-08-22 18:17:49 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> MarcCode </span>{" "}
+            <span className="text-glow text-foreground"> MarCode </span>{" "}
             Portfolio
           </span>
         </a>
